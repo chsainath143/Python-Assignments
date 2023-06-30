f0aa2e8bcc080ed4af6a593f182547ba8ff055db
sainath <saai@saai.com>
2023-06-30 13:16:33 +0000

    Assignment of Day1

---
+++ assignment1.py
@@ -0,0 +1,16 @@
+#create four variables with different datatypes
+a=int(input("Rate letsupgrade from 0 to 10 = "))
+b=input("Enter your review for live sessions =")
+name=input("Enter your name = ")
+print(f"{name} was present for python live session day 1.")
+print(f"Review = {b}")
+print(f"Rating = {a}")
+----------------------------------------
+Output
+
+Rate letsupgrade from 0 to 10 = 8
+Enter your review for live sessions =10
+Enter your name = Chandramahanti Sainath
+Chandramahanti Sainath was present for python live session day 1.
+Review = 10
+Rating = 8
